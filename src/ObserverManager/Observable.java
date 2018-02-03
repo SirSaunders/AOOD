b41404530a8a7c4036e7f92dcbee7d7685e5f4bd
Johnathan Saunders <saundersokj@gmail.com>
2018-02-03 17:49:00 -0500

    reorginized

--- src/ObserverManager/Observable.java
+++
@@ -1,8 +0,0 @@
-package ObserverManager;
-
-/**
- * Created by johnathansaunders on 2/1/18.
- */
-public interface Observable {
-    ObserverManager observerManager =  ObserverManager.getInstance();
-}
